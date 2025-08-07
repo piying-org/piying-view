6f16d05f4296b9ffc6c40ede32939e87860e42de
chen yang <wszgrcy@gmail.com>
2025-08-07 17:15:33 +0800

    test: control

--- projects/view-angular-core/test/field.spec.ts
+++ projects/view-angular-core/test/field.spec.ts
@@ -70,4 +70,29 @@ describe('field', () => {
     let field = await field$.promise;
     expect(field.form.control?.root).toBe(result.form.control);
   });
+  it('getRawValue group', async () => {
+    const obj = v.object({ t1: v.string() });
+    const result = createBuilder(obj);
+    result.form.control!.updateValue({ t1: '1' });
+    result.get(['t1'])?.form.control?.disable();
+    expect(result.form.control?.getRawValue()).toEqual({ t1: '1' });
+  });
+  it('getRawValue array', async () => {
+    const obj = v.array(v.string());
+    const result = createBuilder(obj);
+    result.form.control!.updateValue(['1']);
+    result.fieldArray!()[0].form.control!.disable();
+    expect(result.form.control?.getRawValue()).toEqual(['1']);
+  });
+  it('getRawValue logicGroup', async () => {
+    const field$ = Promise.withResolvers<_PiResolvedCommonViewFieldConfig>();
+    const obj = v.intersect([
+      v.pipe(v.object({ k1: v.string() }), getField(field$)),
+      v.object({ k2: v.string() }),
+    ]);
+    const result = createBuilder(obj);
+    result.form.control!.updateValue({ k1: 'v1' });
+    (await field$.promise).form.control!.disable();
+    expect(result.form.control?.getRawValue()).toEqual({ k1: 'v1' });
+  });
 });
