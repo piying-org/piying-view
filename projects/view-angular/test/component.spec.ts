a7c5ea996b67c622f85461e9adb28943912c34d5
chen yang <wszgrcy@gmail.com>
2025-10-20 16:32:28 +0800

    test

--- projects/view-angular/test/component.spec.ts
+++ projects/view-angular/test/component.spec.ts
@@ -7,12 +7,14 @@ import * as v from 'valibot';
 import {
   NFCSchema,
   patchAsyncClass,
+  patchHooks,
   setOutputs,
 } from '@piying/view-angular-core';
 import { Test1Component } from './test1/test1.component';
 import { setComponent, formConfig } from '@piying/view-angular-core';
 import { NgControl } from '@angular/forms';
 import { TestAttrClassComponent } from './test-attr-class/component';
+import { Subject } from 'rxjs';
 
 describe('组件', () => {
   it('module', async () => {
@@ -153,5 +155,44 @@ describe('组件', () => {
     await fixture.whenStable();
     fixture.detectChanges();
     expect(element.querySelector('input.class1[type=number]')).toBeTruthy();
+  });
+
+  it('组件切换', async () => {
+    const changed = new Subject();
+    const define = v.pipe(
+      v.string(),
+      setComponent('test2'),
+      patchHooks({
+        allFieldsResolved(field) {
+          changed.subscribe(() => {
+            field.define!.update((data) => {
+              return {
+                ...data,
+                type: Test2Component,
+              };
+            });
+          });
+        },
+      }),
+    );
+    const { fixture, instance, element } = await createSchemaComponent(
+      signal(define),
+      signal('d1'),
+
+      {
+        types: {
+          test2: {
+            type: Test1Component,
+          },
+        },
+      },
+    );
+    await fixture.whenStable();
+    fixture.detectChanges();
+    expect(element.querySelector('app-test1')).toBeTruthy();
+    changed.next(1);
+    await fixture.whenStable();
+    fixture.detectChanges();
+    expect(element.querySelector('app-test2')).toBeTruthy();
   });
 });
