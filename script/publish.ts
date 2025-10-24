65b76089cea4579c0200b2bb6defe6675ad3cd35
chen yang <wszgrcy@gmail.com>
2025-10-24 20:30:49 +0800

    1

--- script/publish.ts
+++ script/publish.ts
@@ -14,7 +14,8 @@ async function main() {
   // }
   const TAG = process.env['PUBLISH_TAG'] ?? 'latest';
   await $({ stdio: 'inherit' })`npm run changelog`;
-  await $({ stdio: 'inherit' })`git add changelog`;
+  await $({ stdio: 'inherit' })`ls- lh`;
+  await $({ stdio: 'inherit' })`git add ./changelog`;
   await $({ stdio: 'inherit' })`git commit -m "changelog"`;
   console.log('end');
 
