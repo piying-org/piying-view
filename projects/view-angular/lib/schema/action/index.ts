7e46986f26c5f571da61f1d3cfa56da7dc4396b2
chen yang <wszgrcy@gmail.com>
2025-12-22 21:06:48 +0800

    feat: add export

--- projects/view-angular/lib/schema/action/index.ts
+++ projects/view-angular/lib/schema/action/index.ts
@@ -1,3 +1,6 @@
+import { actions as coreActions } from '@piying/view-angular-core';
+import { directives } from './directive';
+
 export * from './raw-config';
 export {
   asControl,
@@ -6,3 +9,5 @@ export {
   layout,
 } from '@piying/view-angular-core';
 export * from './directive';
+const actions = { ...coreActions, directives };
+export { actions };
