b5e18ca6e70a4b34a7b0e238e6170c11ad432a9f
chen <wszgrcy@gmail.com>
2026-01-17 20:58:12 +0800

    test: selectorless injector

--- projects/view-angular/test/nest.spec.ts
+++ projects/view-angular/test/nest.spec.ts
@@ -6,6 +6,7 @@ import { actions } from '@piying/view-angular-core';
 import { NFCSchema } from '@piying/view-core';
 import { Nest1Service } from './nest/nest1.service';
 import { Nest1Component } from './nest/component';
+import { Nest2Component } from './nest/nest2/component';
 
 describe('nest piying view', () => {
   it('hello', async () => {
@@ -35,6 +36,36 @@ describe('nest piying view', () => {
     fixture.detectChanges();
     expect(element).toBeTruthy();
     let el = element.querySelector('app-nest1');
+    expect(el).toBeTruthy();
+    expect(checked).toBeTrue();
+  });
+  it('selectorless', async () => {
+    let checked = false;
+    const define = v.pipe(
+      NFCSchema,
+      setComponent(Nest2Component),
+      actions.inputs.patch({
+        schema: v.pipe(
+          v.string(),
+          setComponent('test1'),
+          actions.hooks.merge({
+            allFieldsResolved(field) {
+              let service = field.injector.get(Nest1Service);
+              expect(service).toBeTruthy();
+              checked = true;
+            },
+          }),
+        ),
+      }),
+    );
+    const { fixture, instance, element } = await createSchemaComponent(
+      signal(define),
+      signal(undefined),
+    );
+    await fixture.whenStable();
+    fixture.detectChanges();
+    expect(element).toBeTruthy();
+    let el = element.querySelector('app-nest2');
     expect(el).toBeTruthy();
     expect(checked).toBeTrue();
   });
---
+++ projects/view-angular/test/nest/nest2/component.ts
@@ -0,0 +1,34 @@
+import { NgTemplateOutlet } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+} from '@angular/core';
+
+import { PI_INPUT_OPTIONS_TOKEN, PiyingView } from '@piying/view-angular';
+import { Nest1Service } from '../nest1.service';
+import { SelectorlessOutlet } from '@cyia/ngx-common/directive';
+
+@Component({
+  selector: 'app-nest2',
+  templateUrl: './component.html',
+  standalone: true,
+  providers: [Nest1Service],
+  imports: [SelectorlessOutlet],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Nest2Component {
+  readonly PiyingView = PiyingView;
+
+  parentPyOptions = inject(PI_INPUT_OPTIONS_TOKEN);
+  schema = input<any>();
+  schemaOptions$$ = computed(() => {
+    return {
+      schema: this.schema(),
+      options: this.parentPyOptions!(),
+      selectorless: true,
+    };
+  });
+}
