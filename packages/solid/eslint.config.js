2960e1e4295ad4264f3a15879babcd47c4d64268
chen <wszgrcy@gmail.com>
2026-01-18 16:33:28 +0800

    fix: lint

--- packages/solid/eslint.config.js
+++ packages/solid/eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+
 import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
 import unusedImports from 'eslint-plugin-unused-imports';
@@ -10,7 +9,7 @@ export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [js.configs.recommended, tseslint.configs.recommended, reactHooks.configs['recommended-latest'], reactRefresh.configs.vite],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -36,7 +35,6 @@ export default tseslint.config([
       '@typescript-eslint/no-unused-expressions': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
-      'react-refresh/only-export-components': 'off',
     },
   },
 ]);
