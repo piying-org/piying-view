56957f5db35a393e41b9093e7170ee047cb78aeb
chen <wszgrcy@gmail.com>
2026-02-23 19:40:40 +0800

    test: array-to-str

--- projects/view-angular-core/test/transform.spec.ts
+++ projects/view-angular-core/test/transform.spec.ts
@@ -3,6 +3,7 @@ import * as v from 'valibot';
 import { createBuilder } from './util/create-builder';
 import { AbstractControl } from '../field/abstract_model';
 import { FieldControl } from '../../../dist/view-core';
+import { FieldArray } from '../field/field-array';
 describe('transform', () => {
   it('str-to-number', () => {
     const obj = v.object({
@@ -36,5 +37,20 @@ describe('transform', () => {
     expect(v2Field.value).toBe('22');
     expect(result.form.root.value).toEqual({ v1: '22' });
     expect(result.form.root.valid).toBeTrue();
+  });
+  it('array-to-str', () => {
+    const obj = v.object({
+      v1: v.pipe(
+        v.array(v.string()),
+        v.transform((a) => {
+          return a.join(':');
+        }),
+      ),
+    });
+    const result = createBuilder(obj);
+    let v1Field = result.get(['v1'])?.form.control as any as FieldArray;
+    v1Field.updateValue(['1', '2']);
+    expect(v1Field.value).toBe('1:2');
+    expect(v1Field.valid).toBeTrue();
   });
 });
