c660d664fcc0f75505ae0d0d5ea6ae89c4fb4888
chen <wszgrcy@gmail.com>
2026-02-23 21:38:49 +0800

    refactor: getDeepError

--- projects/view-angular-core/util/get-error.ts
+++ projects/view-angular-core/util/get-error.ts
@@ -9,28 +9,6 @@ import { FieldArray } from '../field/field-array';
 import { FieldLogicGroup } from '../field/field-logic-group';
 import { isFieldArray, isFieldGroup } from '../field/is-field';
 
-export function getDeepError(control?: AbstractControl) {
-  if (!control) {
-    return [];
-  }
-  const list = [];
-  const controlList = [control];
-  while (controlList.length) {
-    const control = controlList.shift()!;
-    if (control.valid) {
-      continue;
-    }
-    list.push({ control, errors: control.errors });
-    if (isFieldArray(control) || isFieldGroup(control)) {
-      Object.values(
-        (control.activatedChildren$$ ?? control.children$$)(),
-      ).forEach((control) => {
-        controlList.push(control);
-      });
-    }
-  }
-  return list;
-}
 export interface ErrorSummary {
   pathList: string[];
   fieldList: AbstractControl[];
@@ -82,3 +60,5 @@ export function errorSummary(control?: AbstractControl) {
   }
   return list;
 }
+
+export const getDeepError = errorSummary;
