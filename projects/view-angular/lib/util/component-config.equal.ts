1ec11820a6aab3eb47dd8e3b89967583cbbfdf46
chen <wszgrcy@gmail.com>
2026-03-29 15:39:36 +0800

    remove unuse

--- projects/view-angular/lib/util/component-config.equal.ts
+++ projects/view-angular/lib/util/component-config.equal.ts
@@ -3,19 +3,19 @@ import { DynamicComponentConfig } from '../type/component';
 export function getComponentCheckConfig(a: DynamicComponentConfig) {
   return {
     ...a,
-    inputs: Object.keys(a.inputs() ?? {}).join(','),
+    inputs: Object.keys(a.inputs()).join(','),
     directives: a.directives
       ? a.directives.map((config) => ({
           ...config,
-          inputs: `${config.inputs ? Object.keys(config.inputs() ?? {}).join(',') : ''}`,
+          inputs: `${config.inputs ? Object.keys(config.inputs()).join(',') : ''}`,
         }))
       : undefined,
     // todo 这里是为了schema被替换时,进行比较,感觉应该有更好的方法
-    attributes: Object.entries(a.attributes() ?? {})
+    attributes: Object.entries(a.attributes())
       .map((key, value) => `${key}:${value}`)
       .join(','),
-    events: Object.keys(a.events() ?? {}).join(','),
-    outputs: Object.keys(a.events() ?? {}).join(','),
+    events: Object.keys(a.events()).join(','),
+    outputs: Object.keys(a.outputs()).join(','),
   };
 }
 
