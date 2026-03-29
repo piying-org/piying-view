e3380db0732e6cde1ad45a8ddaebc4f593afa517
chen <wszgrcy@gmail.com>
2026-03-29 11:09:34 +0800

    类型

--- projects/view-angular/lib/type/component.ts
+++ projects/view-angular/lib/type/component.ts
@@ -1,4 +1,4 @@
-import { Injector, Signal, Type } from '@angular/core';
+import { Injector, Type } from '@angular/core';
 import { DirectiveConfig } from '../component/dynamic-define.component';
 import { NgResolvedComponentDefine1 } from './group';
 import {
@@ -8,6 +8,8 @@ import {
   LazyImport,
   LazyMarkType,
   ViewSlots,
+  AsyncObjectSignal,
+  ViewEvents,
 } from '@piying/view-angular-core';
 /** todo 这个没有支持 */
 export interface RawDirectiveOutputs {
@@ -22,12 +24,12 @@ export interface DynamicComponentConfig {
     | LazyImport<NgComponentDefine>
     | LazyMarkType<Type<any>>
     | LazyMarkType<NgComponentDefine>;
-  attributes: Signal<ViewAttributes | undefined>;
-  events: Signal<Record<string, (event: any) => any> | undefined>;
-  inputs: Signal<ViewInputs | undefined>;
-  outputs?: Signal<ViewOutputs>;
-  slots?: Signal<ViewSlots>;
-  directives?: DirectiveConfig[];
+  inputs: AsyncObjectSignal<ViewInputs>;
+  outputs: AsyncObjectSignal<ViewOutputs>;
+  attributes: AsyncObjectSignal<ViewAttributes>;
+  events: AsyncObjectSignal<ViewEvents>;
+  slots: AsyncObjectSignal<ViewSlots>;
+  directives: DirectiveConfig[];
   injector?: Injector;
 }
 
