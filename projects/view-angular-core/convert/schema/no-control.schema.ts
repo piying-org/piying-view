69f082c7442e0bbba84a84668e212409170c6d00
chen <wszgrcy@gmail.com>
2026-03-29 11:57:41 +0800

    nfcComponent

--- projects/view-angular-core/convert/schema/no-control.schema.ts
+++ projects/view-angular-core/convert/schema/no-control.schema.ts
@@ -1,3 +1,7 @@
 import * as v from 'valibot';
+import { setComponent } from '../action';
 /** NonFieldControl */
 export const NFCSchema = v.optional(v.void());
+export function nfcComponent(input: any) {
+  return v.pipe(NFCSchema, setComponent(input));
+}
