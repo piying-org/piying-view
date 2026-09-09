abd1821dfdf9e1fe74c8914f983b903af82b26ae
chen <wszgrcy@gmail.com>
2026-09-09 22:31:57 +0800

    doc

--- nova-docs/src/components/definition/validation-compare.definition.ts
+++ nova-docs/src/components/definition/validation-compare.definition.ts
@@ -1,28 +1,25 @@
 import * as v from 'valibot';
 import { formConfig } from '@piying/view-angular-core';
-import { untracked } from '@angular/core';
 
-export const schema = v.object({
-  password: v.pipe(v.string()),
-  confirmPassword: v.pipe(
-    v.string(),
-    formConfig({
-      validators: [
-        (control) => {
-          return untracked(() => {
-            const parent = control.root;
-            if (parent.value?.password !== control.value) {
-              return [
-                {
-                  kind: 'passwordsNotMatch',
-                  message: '两次密码不一致',
-                },
-              ];
-            }
-            return undefined;
-          });
-        },
-      ],
-    }),
-  ),
-});
+export const schema = v.pipe(
+  v.object({
+    password: v.pipe(v.string()),
+    confirmPassword: v.pipe(v.string()),
+  }),
+  formConfig({
+    validators: [
+      (control) => {
+        const parent = control.root;
+        if (parent.value?.password !== parent.value?.confirmPassword) {
+          return [
+            {
+              kind: 'passwordsNotMatch',
+              message: '两次密码不一致',
+            },
+          ];
+        }
+        return undefined;
+      },
+    ],
+  }),
+);
--- nova-docs/src/components/definition/validation-conditional.definition.ts
+++ nova-docs/src/components/definition/validation-conditional.definition.ts
@@ -1,28 +1,25 @@
 import * as v from 'valibot';
 import { formConfig } from '@piying/view-angular-core';
-import { untracked } from '@angular/core';
 
-export const schema = v.object({
-  hasAddress: v.boolean(),
-  address: v.pipe(
-    v.string(),
-    formConfig({
-      validators: [
-        (control) => {
-          return untracked(() => {
-            const parent = control.root;
-            if (parent.value?.hasAddress && !control.value) {
-              return [
-                {
-                  kind: 'conditionalRequired',
-                  message: '需要填写地址',
-                },
-              ];
-            }
-            return undefined;
-          });
-        },
-      ],
-    }),
-  ),
-});
+export const schema = v.pipe(
+  v.object({
+    hasAddress: v.boolean(),
+    address: v.pipe(v.string()),
+  }),
+  formConfig({
+    validators: [
+      (control) => {
+        const parent = control.root;
+        if (parent.value?.hasAddress && !parent.value?.address) {
+          return [
+            {
+              kind: 'conditionalRequired',
+              message: '需要填写地址',
+            },
+          ];
+        }
+        return undefined;
+      },
+    ],
+  }),
+);
